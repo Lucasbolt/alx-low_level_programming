fcfe8f55ad28d8af3191718a9a4a42626bc6ab44
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-08 09:57:55 -0700

    betty betty"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void) /*just a print function*/
+/**
+ * main -just a print function
+ * Return: returns 0 on success
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
