f10bd7c390832c07424a0d5fded63c7a4d0984bf
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-08 11:18:23 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing to the stderr
+ * Return: returns 1 on success
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is
+useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
