3e0ca3fe8867979f78bfbb6e61541c9115a87fc9
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-09 11:03:17 -0700

    alpHABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,19 @@ int main(void)
 {
 	char c = 'a';
 
+	char c = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
 	while (c <= 'Z')
 	{
 		putchar(c);
 		c++;
 	}
+
 	putchar('\n');
 	return (0);
 }
