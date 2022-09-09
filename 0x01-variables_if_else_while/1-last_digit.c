e8116ff02ca46dcb69b49aac8291786c9c8446e8
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-09 10:13:59 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,6 +18,6 @@ int main(void)
 	else if ((n % 10) > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10));
 	else
-		printf("Last digit of %d is %d and is 0", n, (n % 10))
+		printf("Last digit of %d is %d and is 0", n, (n % 10));
 	return (0);
 }
