a6ca699f00f044176a791c5fe851bf13c18ba016
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-09 13:14:41 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.c>
+
+/**
+ * main - print numbers less than 10
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	for (int a = 0; a < 10; a++)
+		printf("\d\n", a);
+	return (0);
+}
