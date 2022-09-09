f8929a7d72dde02fe90a5569931df98977e5ff1e
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-09 10:39:28 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabets
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar("\n");
+	return (0);
+}
