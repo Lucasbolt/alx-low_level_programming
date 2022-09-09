182e16fd3ad27285f15aae82f8017bdf23431951
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-09 15:35:21 -0700

    brain brain

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int c = 0;
 
-	for (; c < 10; c++)
+	for (; c < 9; c++)
 	{
 		putchar((c % 10) + '0');
-		putchar(' ');
 		putchar(',');
+		putchar(' ');
 	}
-	putchar('\n');
+	putchar((9 % 10) + '0');
 	return (0);
 }
