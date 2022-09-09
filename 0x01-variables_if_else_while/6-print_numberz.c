e4838989adbe20dacb2efa519896ab7f16ca71e5
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-09 14:07:19 -0700

    6-print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	int c = 10;
+
+	for (; c < 10; c++)
+		putchar((c % 10) + '0');
+	putchar('\n');
+	return (0);
+}
