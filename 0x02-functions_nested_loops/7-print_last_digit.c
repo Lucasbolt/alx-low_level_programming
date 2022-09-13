58ee1fd9fa948215aee2f634cee3000a4ecb9001
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-13 07:36:16 -0700

    this program returns the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit -returns the last digit as output
+ * @n: takes an integer argument
+ * Return: returns last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
