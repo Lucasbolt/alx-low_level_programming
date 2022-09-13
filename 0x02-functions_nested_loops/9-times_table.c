2d06f7d1ccd3498588208ad352faf0027f35cf4c
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-13 16:38:33 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,10 +7,11 @@
 void times_table(void)
 {
 	int count = 0;
-	
+
 	for (; count <= 9; count++)
 	{
 		int t[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
 		int c = 0;
 
 		for (; c <= 9; c++)
@@ -34,6 +35,5 @@ void times_table(void)
 			}
 			printf("%d, ", n);
 		}
-
 	}
 }
