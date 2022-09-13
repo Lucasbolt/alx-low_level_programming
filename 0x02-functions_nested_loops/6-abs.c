350e1aaf8c3c4ae07669a5ba353a7ab6d9a4315d
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-13 07:25:06 -0700

    this program returns the absolute values of numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _abs - for calculating absolute values
+ * @n: takes integer arguement n
+ * Return: returns absolute value of n
+ */
+int _abs(int n)
+{
+	return (-n);
+}
