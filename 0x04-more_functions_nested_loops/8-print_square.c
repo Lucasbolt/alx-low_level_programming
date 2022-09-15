174a7476ba97277142ed2ff848ca1c1ee8207d0e
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-15 11:04:22 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @n: size of square
+ */
+void print_square(int n)
+{
+	int m = n;
+
+	for (; n > 0; n--)
+	{
+		int k = m;
+
+		for (; k > 0; k--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
