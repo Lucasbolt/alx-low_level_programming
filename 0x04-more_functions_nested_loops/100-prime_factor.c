7f529e87adc1f4bef068ca1d916096f614e2daa7
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-15 18:18:33 -0700

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the prime factor of a number
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long p = 612852475143, quotient;
+
+	while (quotient < (p / 2))
+	{
+		if ((p % 2) == 0)
+		{
+			p /= 2;
+			continue;
+		}
+
+		for (quotient = 3; quotient < (p / 2); quotient += 2)
+		{
+			if ((p % quotient) == 0)
+				p /= quotient;
+		}
+	}
+
+	printf("%ld\n", p);
+
+	return (0);
+}
