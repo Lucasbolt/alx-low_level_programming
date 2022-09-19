01b2528fe7d761e73449be182da3242412c6333f
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-19 10:33:32 -0700

    fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * reset_to_98 - updates the value of pointer
- * @*n: takes a pointer as an argument
+ * @n: takes a pointer as an argument
  */
 void reset_to_98(int *n)
 {
