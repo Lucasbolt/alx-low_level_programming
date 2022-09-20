8abe66a4e3be811a43cc5f4caeb9a056e5c458f3
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-19 17:42:04 -0700

    print half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,17 @@
  */
 void puts_half(char *str)
 {
-	int c = 0, i = 0;
+	int c = 0, i = 0, n;
 
 	while (str[i++])
 		c++;
-	c = c / 2;
+	if ((c % 2) == 0)
+		n = c / 2;
+	
+	else
+		n = (c + 1) / 2;
 
-	for (i = 0; i <= c; i++)
+	for (i = n; i <= c; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
