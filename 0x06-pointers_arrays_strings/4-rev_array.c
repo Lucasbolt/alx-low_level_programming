5a51f0b82eb42cfb5d370f3159624d7cff12fafa
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-22 09:00:27 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverses array
+ * @a: number array pointer
+ * @n: number of array
+ */
+void reverse_array(int *a, int n)
+{
+	int b[98];
+	int count, len = n;
+	n = n -1;
+
+	for (count = 0; count < len; count++)
+		b[count] = a[n--];
+
+	for (count = 0; count < len; count++)
+		a[count] = b[count];
+
+}
