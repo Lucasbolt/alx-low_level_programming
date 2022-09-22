c4902e485122b41ce33c5f5a6beae21b0e5525e2
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-22 00:47:43 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenates @src to @dest with at most @n character from @src
+ * @dest: first input and destination string
+ * @src: second input and source string
+ * @n: max number of @src characters to concatenate
+ * Return: returns a pointer to @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0, index = 0;
+
+	while (dest[index++])
+		count++;
+
+	for (index = 0; index < n; index++)
+	{
+		dest[count] = src[index];
+		count++;
+	}
+	return (dest);
+}
