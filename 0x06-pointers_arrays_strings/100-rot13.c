b7b1d95aee89fdadac9aaa94265f5d87caf36c7b
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-22 15:41:46 -0700

    rot13 encoding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes strings into rot13
+ * @a: string input
+ * Return: returns pointer to @a
+ */
+char *rot13(char *a)
+{
+	int count1 = 0, count2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (a[count1])
+	{
+		for (count2 = 0; count2 < 52; count2++)
+		{
+			if (a[count1] == alphabet[count2])
+			{
+				a[count1] = rot13key[count2];
+				break;
+			}
+		}
+
+		count1++;
+	}
+
+	return (a);
+}
+
