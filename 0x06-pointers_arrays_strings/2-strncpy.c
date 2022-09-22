1b628d820d661bbd2efbf74692a6c4f61a2b0afb
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-22 07:09:07 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * _strncpy - replaces [0-n] characters of @dest arrays with [0-n] characters of @src
+ * _strncpy - copies [0-n] characters of @dest arrays with [0-n]
+ *	characters of @src
  * @dest: first input - destination array
  * @src: second input - src array
  * @n: max number of characters to copy
@@ -12,11 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0, count = 0;
 
-	while(src[index++])
+	while (src[index++])
 		count++;
 
-	for (index = 0;src[index] && index < n; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
+
 	for (index = count; index < n; index++)
 		dest[index] = '\0';
 
