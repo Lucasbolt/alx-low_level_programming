52c30abeebd96cd8acb22c8c214306378d62c692
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-26 08:26:21 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence of substring @needle
+ *	in the string @haystack
+ * @haystack: main string
+ * @needle: strings to search for
+ * Return: returns a pointer to the beginning of the located
+ *	substring or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index1;
+
+	for (index1 = 0; haystack[index1]; index1++)
+	{
+		if (haystack[index1] == needle[0])
+			return (haystack + index1);
+	}
+	return ('\0');
+}
