535c90acaf0d1f367d551ab3e6cd2156c86ea609
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-09-30 12:52:12 -0700

    add numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char **argv)
 {
-	int num, c, sum = 0;
+	int num, c, b, sum = 0;
 
 	if (argc < 2)
 	{
@@ -18,15 +18,17 @@ int main(int argc, char **argv)
 	}
 	for (c = 1; c < argc; c++)
 	{
-		num = atoi(argv[c]);
-		if (num == 0)
+		for (b = 0; argv[c][b]; b++)
 		{
-			puts("Error");
-			return (1);
+			if (argv[c][b] < '0' || argv[c][b] > '9')
+			{
+				puts("Error");
+				return (1);
+			}
 		}
-		else
-			sum += num;
-		}
+		num = atoi(argv[c]);
+		sum += num;
+	}
 	printf("%d\n", sum);
 
 	return (0);
