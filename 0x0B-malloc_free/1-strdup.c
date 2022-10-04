fb3f8c6ca0dc2345a98aae9baa150809a6a70acd
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-10-04 01:06:12 -0700

    allocate space and copy str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strdup - allocate memory size of @str and copy @str into it.
+ * @str: string to be copied
+ * Return: return pointer to copied strin
+ *	returns NULL if failed.
+ */
+char *_strdup(char *str)
+{
+	int c, i;
+	char *p;
+
+	c = strlen(str);
+
+	p = (char *) malloc(c * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+	if (p != NULL)
+	{
+		for (i = 0; i < c; i++)
+		{
+			p[i] = str[i];
+		}
+	}
+	return (p);
+}
