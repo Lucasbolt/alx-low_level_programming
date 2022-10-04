1f3f02ac78f6dc396dce33e8a2dd9421a37789f7
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-10-04 05:40:25 -0700

    allocate mem and creates 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - allocate mem for 2-dimensional array
+ * @width: width of array
+ * @height: height of array
+ * Return: return pointer to array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int a, b;
+
+	if (width <= 0)
+		return (NULL);
+	if (height <= 0)
+		return (NULL);
+
+	p = malloc(height * sizeof(int *));
+
+	if (p)
+	{
+		for (a = 0; a < height; a++)
+		{
+			p[a] = malloc(sizeof(int) * width);
+			
+			if (p[a] == NULL)
+			{
+				for (; a <= 0; a--)
+					free(p[a]);
+				free(p);
+				return (NULL);
+			}
+		}
+
+		for (a = 0; a < height; a++)
+		{
+			for (b = 0; b < width; b++)
+				p[a][b] = 0;
+		}
+	}
+	return (p);
+}
