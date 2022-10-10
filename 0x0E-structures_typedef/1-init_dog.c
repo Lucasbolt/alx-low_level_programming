811a84437514f46000b53d7f02105f2483633a3c
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-10-10 09:52:53 -0700

    initialize dog structs

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initializes dog info
+ * @d: struct defining dof structure
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
