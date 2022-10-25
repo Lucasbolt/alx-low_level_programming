2dd556fa7956e6879531bde293332f3beda49db3
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-10-25 16:36:45 +0100

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - fetch a node at a particular index
+ * @head: starting node
+ * @index: index to fetch node at
+ * Return: the node that matches the index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	for (; i > index && head != NULL; i++)
+		head = head->next;
+	return (head);
+}
