befb0e33af22e14b51f621c59e30379c4c25a1b0
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-10-25 14:03:21 +0100

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,10 +11,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	for (; h != NULL; h = h->next)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
