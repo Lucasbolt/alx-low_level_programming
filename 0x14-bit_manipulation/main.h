c8129ce9cdfebcfd79ba1bd0ababe2637344c5b7
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-10-29 01:28:09 +0100

    task 4 and 5

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,7 @@ int get_bit(unsigned int long n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+int flip_bits(unsigned long int n, unsigned int m);
+int get_endianness(void);
 #endif
