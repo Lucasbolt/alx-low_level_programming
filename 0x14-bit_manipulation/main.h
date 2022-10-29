839e196f641c56680037a33474f51200e627e20c
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-10-29 01:15:35 +0100

    task 3

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@ int _putchar(char n);
 int get_bit(unsigned int long n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int set_bit(unsigned long int n, unsigned int index);
 #endif
