dc381babd5710891a9011f227095d3492833368e
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-10-29 01:06:06 +0100

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - fetches the bit at a particular index
+ * @n: number
+ * @index: index
+ * Return: the bit
+ */
+int get_bit(unsigned int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char n);
+int get_bit(unsigned int long n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 #endif
