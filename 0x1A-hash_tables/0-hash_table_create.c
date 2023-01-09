a9abda2b03e1df0053a2b36978f0fe1849d9ec5d
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-01-09 07:11:23 +0100

    task 0- creating a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table of the size @size.
+ * @size: size of table to be initialized.
+ * return: returns pointer to the created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *htable;
+	hash_node_t **Array;
+	unsigned long int i;
+
+	htable = malloc(sizeof(hash_table_t));
+	if (htable == NULL)
+		return (NULL);
+
+	Array = malloc(sizeof(hash_node_t *) * size);
+	
+	for (i = 0; i < size; i++)
+		Array[i] = NULL;
+	htable->size = size;
+	htable->array = Array;
+
+	return (htable);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This directory contains practice codes @ALX implementing hash functions h tables using C programming language.
