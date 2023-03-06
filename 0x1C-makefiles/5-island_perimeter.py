d5898cd69332928ebafa9556a3a68b80f72439ff
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-03-06 15:31:24 +0100

    advanced task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,23 +7,22 @@ Defines the island perimeter calculator.
 def island_perimeter(grid):
     """
     function for calculating the perimeter of an island given as grid.
-    grid is a list of list of integers with 0s representing water and 
+    grid is a list of list of integers with 0s representing water and
     1's representing land.
     """
     perimeter = 0
     if len(grid) == 0:
         return perimeter
     rows, cols = len(grid), len(grid[0])
-    
-    for l in range(rows):
-        for c in range(cols):
-            if grid[l][c] == 1:
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
                 perimeter += 4
 
-                if l > 0:
-                    if grid[l-1][c] == 1:
+                if row > 0:
+                    if grid[row-1][col] == 1:
                         perimeter -= 2
-                if c > 0:
-                    if grid[l][c-1] == 1:
+                if col > 0:
+                    if grid[row][col-1] == 1:
                         perimeter -= 2
     return perimeter
---
+++ 0x1C-makefiles/m.h
@@ -0,0 +1,7 @@
+#ifndef __MH__
+#define __MH__
+
+#include <stdio.h>
+#include <stdlib.h>
+void print_school(void);
+#endif
